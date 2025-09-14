bb730bc53f73f2e6fc599d61dea1df804fad0ee1
Людмила Бубенко <Tyuiu.bubenkolg@outlook.com>
2025-09-14 22:31:31 +0500

    Add project files.

---
+++ Tyuiu.BubenkoLG.Sprint1.Task0.V14.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BubenkoLG.Sprint1.Task0.V14.Lib;
+
+namespace Tyuiu.BubenkoLG.Sprint1.Task0.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(25, res);
+        }
+    }
+}
---
+++ Tyuiu.BubenkoLG.Sprint1.Task0.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BubenkoLG.Sprint1.Task0.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BubenkoLG.Sprint1.Task0.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8548a0d8-a5fa-4ec1-a50d-5f0c98ea50d0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
