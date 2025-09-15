477b87fe2128e2cd50293fefdf3899974b5015da
Людмила Бубенко <Tyuiu.bubenkolg@outlook.com>
2025-09-15 19:16:22 +0500

    Task1

---
+++ Tyuiu.BubenkoLG.Sprint1.Task1.V14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BubenkoLG.Sprint1.Task1.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+
+            double f = 7.5;
+
+            double dbl = 10.01;
+
+            Console.WriteLine("i=" + i + "\\f=" + f + "\\dbl=" + dbl);
+        }
+        
+    }
+}
---
+++ Tyuiu.BubenkoLG.Sprint1.Task1.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BubenkoLG.Sprint1.Task1.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BubenkoLG.Sprint1.Task1.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("81ded3ec-d488-4b6f-ae95-ebb77046812f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
