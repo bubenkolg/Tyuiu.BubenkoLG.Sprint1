ac9bdf50280099f53100d851f0cc881d9b1c3d2f
Людмила Бубенко <Tyuiu.bubenkolg@outlook.com>
2025-09-18 00:01:32 +0500

    Task2

---
+++ Tyuiu.BubenkoLG.Sprint1.Task2.V17.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BubenkoLG.Sprint1.Task2.V17.Lib;
+
+namespace Tyuiu.BubenkoLG.Sprint1.Task2.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 60;
+            var res = ds.ConvertMinutesToHours(x);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.BubenkoLG.Sprint1.Task2.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BubenkoLG.Sprint1.Task2.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BubenkoLG.Sprint1.Task2.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c8c556ce-f8f0-4b32-b8d1-4b7c36bf30c8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
