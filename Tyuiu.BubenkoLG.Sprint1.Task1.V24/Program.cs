77625bc58fa26650bf8f5bcb6c8856f7edcf2459
Людмила Бубенко <Tyuiu.bubenkolg@outlook.com>
2025-09-21 19:55:42 +0500

    Task1

---
+++ Tyuiu.BubenkoLG.Sprint1.Task1.V24/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BubenkoLG.Sprint1.Task1.V24.Lib;
+
+
+namespace Tyuiu.BubenkoLG.Sprint1.Task1.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Бубенко Л. Г. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                ");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                         ");
+            Console.WriteLine("* Задание #1                                                               ");
+            Console.WriteLine("* Вариант #24                                                              ");
+            Console.WriteLine("* Выполнила: Бубенко Людмила Георгиевна | СМАРТб-25-1                      ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 ");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,  ");
+            Console.WriteLine("* вычисляет результат по формуле (1-x)/(2+y) и печатает его на экране.     ");
+            Console.WriteLine("*                                                                          ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("* (1-x)/(2+y)                                                              ");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x,y));
+            Console.ReadKey();
+        }
+    }
+}
