f8a98225f3d156601d9829b700a1af5049d72576
Людмила Бубенко <Tyuiu.bubenkolg@outlook.com>
2025-09-27 22:22:30 +0500

    Task4

---
+++ Tyuiu.BubenkoLG.Sprint1.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BubenkoLG.Sprint1.Task4.V29.Lib;
+
+namespace Tyuiu.BubenkoLG.Sprint1.Task4.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 5;
+            var res = ds.Calculate(x,y);
+            double wait = 0.007;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BubenkoLG.Sprint1.Task4.V29/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BubenkoLG.Sprint1.Task4.V29.Lib;
+
+
+namespace Tyuiu.BubenkoLG.Sprint1.Task4.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Бубенко Л. Г. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                ");
+            Console.WriteLine("* Тема: Class Math                                                         ");
+            Console.WriteLine("* Задание #1                                                               ");
+            Console.WriteLine("* Вариант #29                                                              ");
+            Console.WriteLine("* Выполнила: Бубенко Людмила Георгиевна | СМАРТб-25-1                      ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 ");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,  ");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                 ");
+            Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                               ");
+            Console.WriteLine("*                                                                          ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("* Два числа.                                                               ");
+            Console.WriteLine("***************************************************************************");
+
+            double x,y;
+            Console.WriteLine("Введите X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x,y));
+            Console.ReadKey();
+        }
+    }
+}
